8cb9b08fe537e1a4632601987b21895f6ad4c3bc
Teo Bicchieri <teobicchieri@yahoo.com>
2019-07-21 21:47:15 -0700

    removed bodyParser call from app and fixted doctype on about.ejs. still trying to get app to open online..

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs"); //this sets the default to look for ejs files
 
