9942e4c70204b0090585992ffedf0367291f2a1a
Teo Bicchieri <teobicchieri@yahoo.com>
2019-07-21 21:10:52 -0700

    work on app.js..initial deploy not working

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ app.use(express.static("public"));
 app.set("view engine", "ejs"); //this sets the default to look for ejs files
 
 
+
+
+// BODY CODE
+
+//basic / req / 
+app.get("/", function(req, res){
+	res.render("home");
+}); //takes user to the home page
+
+app.get("/main", function(req, res){
+    res.render("main")
+}); //takes user to the main page
+
+app.get("/about", function(req, res){
+    res.render("about");
+}); //takes user to about page
+
+app.get("/selector", function(req, res){
+    res.render("selector");
+}); //shows the selector tool form
+
 var safaris = [
     {name: "Highvelt Hunting Safari", id: "dry", class: "medium" },
     {name: "Desert Survival Safari", id: "dry", class: "short"},
@@ -38,27 +59,7 @@ function myFunction() {
 
     window.location.href = "mailto:" + email + "?body=" + body;
   }
- 
-
-// BODY CODE
-
-//basic / req / 
-app.get("/", function(req, res){
-	res.render("home");
-}); //takes user to the home page
-
-app.get("/main", function(req, res){
-    res.render("main")
-}); //takes user to the main page
-
-app.get("/about", function(req, res){
-    res.render("about");
-}); //takes user to about page
-
-app.get("/selector", function(req, res){
-    res.render("selector");
-}); //shows the selector tool form
-	
+ 	
 								
 	//set up listener for requests
 const port = process.env.PORT;
